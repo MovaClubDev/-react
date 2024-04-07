b1cb1078c8dc0bf2debe45a3b586fd9b949b5aff
MovaDev <dev@mova.club>
2024-04-07 21:06:01 +0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import IMAGE from '../assets/kek.jpg';
+
+export default function App() {
+
+    return(
+    <>
+        <h3>This is React APP</h3>
+        <img src={IMAGE} alt="SHLYAPA" width="960" height="540"/>
+    </>)
+}
